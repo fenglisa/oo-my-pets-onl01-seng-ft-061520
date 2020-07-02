666e29e7bd0f2dd336428ea9c779299f5369b80b
Lisa Feng <lisafeng10@gmail.com>
2020-07-02 01:35:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,7 +59,13 @@ class Owner
   end
   
   def sell_pets
-    self.dogs.each {|dog| dog.mood = "nervous"}
-    self.cats.each {|cat| cat.mood = "nervous"}
+    self.dogs.each {|dog| 
+      dog.mood = "nervous"
+      dog.owner = nil
+    }
+    self.cats.each {|cat| 
+      cat.mood = "nervous"
+      cat.owner = nil
+    }
   end
 end
